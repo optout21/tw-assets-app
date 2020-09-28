38118e734b5efcb3b02f4f4e49af833c12df13a1
Catenocrypt <catenocrypt@users.noreply.github.com>
2020-09-28 15:28:10 +0200

    Rename, script-index

--- src/script-index.js
+++ src/script-index.js
@@ -1,7 +1,7 @@
 // Collection of dependency libs, to be bundled
 
 import * as assetsLib from "../assetsLib/dist/assetsLib.js";
-
+const octokit = require("@octokit/request");
 export const assets = assetsLib;
-export const octoRequest = require("@octokit/request").request;
+export const octoRequest = octokit.request;
 
