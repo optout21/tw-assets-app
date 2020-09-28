4a0a7e8f359a1900847d7a23e18131d1825a4ab1
Catenocrypt <catenocrypt@users.noreply.github.com>
2020-09-28 15:17:13 +0200

    Simplfy exports.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,6 @@
 
 import * as assetsLib from "../assetsLib/dist/assetsLib.js";
 
-export const assets = {
-    TokenInfo: assetsLib.TokenInfo,
-    TokenInput: assetsLib.TokenInput,
-    tokenIdsFromFiles: assetsLib.tokenIdsFromFiles,
-    tokenInfoOfExistingToken: assetsLib.tokenInfoOfExistingToken,
-    tokenInfoOfExistingTokenInRepo: assetsLib.tokenInfoOfExistingTokenInRepo,
-    checkTokenInput: assetsLib.checkTokenInput,
-    chainFromType: assetsLib.chainFromType
-};
+export const assets = assetsLib;
 export const octoRequest = require("@octokit/request").request;
 
