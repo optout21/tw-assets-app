50108a76ea1c15e6a209ad99a8cb144bafdd8585
Catenocrypt <catenocrypt@users.noreply.github.com>
2020-09-28 15:14:13 +0200

    Expose assetsLib through script bundle.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 // Collection of dependency libs, to be bundled
 
+import * as assetsLib from "../assetsLib/dist/assetsLib.js";
+
+export const assets = {
+    TokenInfo: assetsLib.TokenInfo,
+    TokenInput: assetsLib.TokenInput,
+    tokenIdsFromFiles: assetsLib.tokenIdsFromFiles,
+    tokenInfoOfExistingToken: assetsLib.tokenInfoOfExistingToken,
+    tokenInfoOfExistingTokenInRepo: assetsLib.tokenInfoOfExistingTokenInRepo,
+    checkTokenInput: assetsLib.checkTokenInput,
+    chainFromType: assetsLib.chainFromType
+};
 export const octoRequest = require("@octokit/request").request;
+
