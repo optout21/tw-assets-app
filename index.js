2d2edd5344cc7d774037879e662c83cfbc283808
Catenocrypt <catenocrypt@users.noreply.github.com>
2020-10-05 10:12:35 +0200

    Cleanup index.js

--- index.js
+++ index.js
@@ -8,44 +8,14 @@ const http = require('http');
 const url = require('url');
 const finalhandler = require('finalhandler');
 const serveStatic = require('serve-static');
-const dotenv = require('dotenv');
-const fetch = require('node-fetch');
-//const { octokit } = require("@octokit/rest");
-//const { createOAuthAppAuth } = require("@octokit/auth");
+//const dotenv = require('dotenv');
 const { createOAuthAppAuth } = require("@octokit/auth-oauth-app");
-const appName = "tw-assets-management";
-
-var access_token = null;
-
-function log (text) {
-    console.log('[' + appName + ' ' + (new Date()).toISOString() + ']: ' + text);
-}
 
 const clientId = "b9ff96b9717574ee8189";
 const clientSecret = "fd107ac0d0a52c3513b7e29bf10e528079b334fe";
+const port = 3000;
 
-async function retrieveToken(code) {
-    const url = "https://github.com/login/oauth/access_token";
-    let resp = await fetch(url, {
-        method: "POST",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            client_id: clientId,
-            client_secret: clientSecret,
-            code: code
-        })
-    });
-    if (resp.status != 200) {
-        alert(`Error from ${url}, status ${resp.status} ${resp.statusText}`);
-        return null;
-    }
-    const respJson = await resp.json();
-    //console.log(respJson);
-    return respJson["access_token"];
-}
+var access_token = null;
 
 async function handleCallback(request, response) {
     console.log(`Callback url ${request.url}`);
@@ -64,12 +34,6 @@ async function handleCallback(request, response) {
         clientSecret: clientSecret,
         code: code
     });
-    /*
-    const appAuthentication = await auth({
-        type: "oauth-app",
-    });
-    console.log(JSON.stringify(appAuthentication));
-    */
     const tokenAuthentication = await auth({
         type: "token",
         code: code,
@@ -92,85 +56,21 @@ async function handleCallback(request, response) {
     response.end('');
 }
 
-// Runs right after startup
-async function start() {
-    dotenv.config();
-
-    log('App ' + appName + ' started')
-
-    if (!process.env.GITHUB_APP_IDENTIFIER) { log('ERROR: Missing GITHUB_APP_IDENTIFIER in env'); return; }
-    if (!process.env.GITHUB_PRIVATE_KEY) { log('ERROR: Missing GITHUB_PRIVATE_KEY in env'); return; }
-    //if (!process.env.APP_INSTANCE_ID) { log('ERROR: Missing APP_INSTANCE_ID in env'); return }
-    if (!process.env.REPO_OWNER) { log('ERROR: Missing REPO_OWNER in env'); return }
-    if (!process.env.REPO_NAME) { log('ERROR: Missing REPO_NAME in env'); return; }
-    if (!process.env.GITHUB_WEBHOOK_SECRET) { log('ERROR: Missing GITHUB_WEBHOOK_SECRET in evn'); return; }
-
-    const appInstallationId = 11830095
-    process.env.APP_INSTANCE_ID = appInstallationId;
-    log(`APP_INSTANCE_ID: ${process.env.APP_INSTANCE_ID}`);
-    const owner = process.env.REPO_OWNER;
-    const repo = process.env.REPO_NAME;
-    log(`owner: ${owner}  repo: ${repo}`);
-
-    // remove new lines
-    const privateKey = process.env.GITHUB_PRIVATE_KEY.replace("\\n", "\n");
-
-    const options = {
-        id: process.env.GITHUB_APP_IDENTIFIER,
-        privateKey: privateKey,
-        secret: process.env.GITHUB_WEBHOOK_SECRET
-    };
-    /*
-    export interface Options {
-        privateKey?: string;
-        githubToken?: string;
-        id?: number;
-        Octokit?: typeof ProbotOctokit;
-        log?: Logger;
-        redisConfig?: Redis.RedisOptions;
-        secret?: string;
-        webhookPath?: string;
-        cache?: LRUCache<number, string>;
-        octokit?: InstanceType<typeof ProbotOctokit>;
-        throttleOptions?: any;
-        webhooks?: Webhooks;
-    }
-    */
-    var app = new probot.Application(options);
-    gitHubApi = await app.auth(appInstallationId, null);
-    if (!gitHubApi) { log('ERROR: Could not obtain gitHubApi'); return; }
-
-    // create a context on our own
-    defaultContext = {
-        payload: {
-            repository: {
-                name: repo,
-                full_name: owner + '/' + repo,
-                owner: {
-                    login: owner
-                }
-            }
-        },
-        github: gitHubApi
-    }
-
-    const prs = await queryPulls(gitHubApi, defaultContext);
-    console.log('prs', prs);
-}
-
-//start();
-
 var serve = serveStatic("./static-files");
 
 var httpServer = http.createServer(async function (req, res) {
-    console.log(`req.url ${req.url}`)
-    //res.writeHead(200, {'Content-Type': 'text/plain'});
-    //res.end('Hello World!');
-    if (req.url === "/callback" || req.url.startsWith("/callback?")) {
-        handleCallback(req, res);
-        return;
+    try {
+        console.log(`req.url ${req.url}`)
+        if (req.url === "/callback" || req.url.startsWith("/callback?")) {
+            handleCallback(req, res);
+            return;
+        }
+        var done = finalhandler(req, res);
+        serve(req, res, done);
+    } catch (error) {
+        console.log("ERROR: ", error);
     }
-    var done = finalhandler(req, res);
-    serve(req, res, done);
 });
-httpServer.listen(3000); 
+
+console.log("About to listen, port", port);
+httpServer.listen(port);
