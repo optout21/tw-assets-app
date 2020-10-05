a1fa345291839eb47089480d2f1743b7a254d6a2
Catenocrypt <catenocrypt@users.noreply.github.com>
2020-10-05 10:46:01 +0200

    docker.sh comments

--- docker.sh
+++ docker.sh
@@ -4,8 +4,11 @@ TAG=trustwallet/assets-management
 npm run build
 
 docker build -t $TAG .
+#docker build -t $TAG:<version> .
 
 #docker run -p 49170:3000 -d $TAG
+#docker push $TAG
+#docker push $TAG:<version>
 
 #docker ps
 #docker logs <container id>
