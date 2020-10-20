21fe640426025304149721dec218da92bda818f5
Adam R <13562139+catenocrypt@users.noreply.github.com>
2020-10-20 10:33:40 +0200

    Add check for token circulation (#4)
    * Format
    
    * Add check for token circulation.
    
    Co-authored-by: Catenocrypt <catenocrypt@users.noreply.github.com>

--- assetsLib/src/tokenInfo.ts
+++ assetsLib/src/tokenInfo.ts
@@ -31,7 +31,7 @@ export function explorerUrl(type: string, contract: string): string {
                 return `https://explorer.binance.org/asset/${contract}`;
             case "bep20":
                 return `https://bscscan.com/token/${contract}`;
-            }
+        }
     }
     return "";
 }
@@ -145,7 +145,7 @@ export function tokenIdsFromFiles(filenames: string[]): [string, string][] {
 }
 
 export interface ImageDimensionsCalculator {
-    get(imageUrl: string, imageStream: string): Promise<{x: number, y: number}>;
+    get(imageUrl: string, imageStream: string): Promise<{ x: number, y: number }>;
 }
 
 // Check tokenInfo for validity: contract is OK, logo is OK, etc.
@@ -153,96 +153,100 @@ export interface ImageDimensionsCalculator {
 // - result: 0 for all OK, 1 for at least on warning, 2 for at least on error
 // - a multi-line string with the detailed results
 export async function checkTokenInfo(tokenInfo: TokenInfo, imgDimsCalc: ImageDimensionsCalculator): Promise<[number, string]> {
-    let res: {res: number, msg: string}[] = [];
+    let res: { res: number, msg: string }[] = [];
 
     if (!tokenInfo.type || !normalizeType(tokenInfo.type)) {
-        res.push({res: 2, msg: `Invalid token type ${tokenInfo.type}`});
+        res.push({ res: 2, msg: `Invalid token type ${tokenInfo.type}` });
     } else {
-        res.push({res: 0, msg: `Token type OK (${tokenInfo.type})`});
+        res.push({ res: 0, msg: `Token type OK (${tokenInfo.type})` });
     }
 
     res.push(checkTokenInfoContract(tokenInfo));
 
     (await checkTokenInfoLogo(tokenInfo, imgDimsCalc)).forEach(r => res.push(r));
-    
+
     if (!tokenInfo.info) {
-        res.push({res: 2, msg: "Info.json must not be missing"});
+        res.push({ res: 2, msg: "Info.json must not be missing" });
     } else {
         if (!tokenInfo.info["website"]) {
-            res.push({res: 2, msg: "Website cannot be empty"});
+            res.push({ res: 2, msg: "Website cannot be empty" });
         } else {
             const website = tokenInfo.info["website"];
             try {
                 const result = await fetch(website);
                 if (result.status != 200) {
-                    res.push({res: 2, msg: `Website does not exist, status ${result.status}, url ${website}`});
+                    res.push({ res: 2, msg: `Website does not exist, status ${result.status}, url ${website}` });
                 } else {
-                    res.push({res: 0, msg: `Website OK`});
+                    res.push({ res: 0, msg: `Website OK` });
                 }
             } catch (error) {
-                res.push({res: 2, msg: `Website does not exist, error ${error}, url ${website}`});
+                res.push({ res: 2, msg: `Website does not exist, error ${error}, url ${website}` });
             }
         }
 
         if (!tokenInfo.info["explorer"]) {
-            res.push({res: 2, msg: "Explorer cannot be empty"});
+            res.push({ res: 2, msg: "Explorer cannot be empty" });
         } else {
             const explorerUrl = tokenInfo.info["explorer"];
             try {
                 const result = await fetch(explorerUrl);
                 if (result.status != 200) {
-                    res.push({res: 2, msg: `ExplorerUrl does not exist, status ${result.status}, url ${explorerUrl}`});
+                    res.push({ res: 2, msg: `ExplorerUrl does not exist, status ${result.status}, url ${explorerUrl}` });
                 } else {
-                    res.push({res: 0, msg: `Explorer URL OK`});
+                    res.push({ res: 0, msg: `Explorer URL OK` });
                 }
             } catch (error) {
-                res.push({res: 2, msg: `ExplorerUrl does not exist, error ${error}, url ${explorerUrl}`});
+                res.push({ res: 2, msg: `ExplorerUrl does not exist, error ${error}, url ${explorerUrl}` });
             }
         }
 
         if (!tokenInfo.info["short_description"]) {
-            res.push({res: 2, msg: "Short description cannot be empty"});
+            res.push({ res: 2, msg: "Short description cannot be empty" });
         } else {
-            res.push({res: 0, msg: "Description OK"});
+            res.push({ res: 0, msg: "Description OK" });
         }
     }
 
+    // circulation
+    const holders = await getTokenCirculation(tokenInfo.type, tokenInfo.info["explorer"], tokenInfo.contract);
+    res.push(checkHoldersLimit(holders));
+    
     return AggregateCheckResults(res);
 }
 
 // Aggregate results: max and string
-export function AggregateCheckResults(res: {res: number, msg: string}[]): [number, string] {
+export function AggregateCheckResults(res: { res: number, msg: string }[]): [number, string] {
     let maxres = 0;
     let msg = "";
     res.forEach(r => maxres = Math.max(r.res, maxres));
     // Error first, warnings, OKs
-    res.forEach(r => {if (r.res >= 2) { msg += "❌  " + r.msg + "\n"; }});
-    res.forEach(r => {if (r.res == 1) { msg += "!  " + r.msg + "\n"; }});
-    res.forEach(r => {if (r.res == 0) { msg += "✓  " + r.msg + "\n"; }});
+    res.forEach(r => { if (r.res >= 2) { msg += "❌  " + r.msg + "\n"; } });
+    res.forEach(r => { if (r.res == 1) { msg += "!  " + r.msg + "\n"; } });
+    res.forEach(r => { if (r.res == 0) { msg += "✓  " + r.msg + "\n"; } });
     return [maxres, msg];
 }
 
-function checkTokenInfoContract(tokenInfo: TokenInfo): {res: number, msg: string} {
+function checkTokenInfoContract(tokenInfo: TokenInfo): { res: number, msg: string } {
     if (!tokenInfo.contract) {
-        return {res: 2, msg: "Contract/ID cannot be empty"};
+        return { res: 2, msg: "Contract/ID cannot be empty" };
     }
     if (tokenInfo.type.toLowerCase() === "erc20" || tokenInfo.type.toLowerCase() === "bep20") {
         if (!isEthereumAddress(tokenInfo.contract)) {
-            return {res: 2, msg: `Contract is not a valid Ethereum address!`};
+            return { res: 2, msg: `Contract is not a valid Ethereum address!` };
         }
         const inChecksum = toChecksum(tokenInfo.contract);
         if (inChecksum !== tokenInfo.contract) {
-            return {res: 2, msg: `Contract is not in checksum format, should be ${inChecksum}`};
+            return { res: 2, msg: `Contract is not in checksum format, should be ${inChecksum}` };
         }
     }
-    return {res: 0, msg: `Contract/ID is OK`};
+    return { res: 0, msg: `Contract/ID is OK` };
 }
 
-async function checkTokenInfoLogo(tokenInfo: TokenInfo, imgDimsCalc: ImageDimensionsCalculator): Promise<{res: number, msg: string}[]> {
-    let res: {res: number, msg: string}[] = [];
+async function checkTokenInfoLogo(tokenInfo: TokenInfo, imgDimsCalc: ImageDimensionsCalculator): Promise<{ res: number, msg: string }[]> {
+    let res: { res: number, msg: string }[] = [];
 
     if (!tokenInfo.logoStream && !tokenInfo.logoUrl) {
-        return [{res: 2, msg: "Logo image may not be missing"}];
+        return [{ res: 2, msg: "Logo image may not be missing" }];
     }
     let logoStreamSize = tokenInfo.logoStreamSize;
     let logoStreamType = tokenInfo.logoStreamType;
@@ -250,39 +254,96 @@ async function checkTokenInfoLogo(tokenInfo: TokenInfo, imgDimsCalc: ImageDimens
         try {
             const response = await fetch(tokenInfo.logoUrl);
             if (response.status != 200) {
-                return [{res: 2, msg: `Could not retrieve logo from url ${tokenInfo.logoUrl}, status ${status}`}];
+                return [{ res: 2, msg: `Could not retrieve logo from url ${tokenInfo.logoUrl}, status ${status}` }];
             }
             logoStreamSize = (await response.arrayBuffer()).byteLength;
             logoStreamType = response.headers.get('Content-Type');
         } catch (error) {
-            return [{res: 2, msg: `Could not retrieve logo from url ${tokenInfo.logoUrl}, error ${error}`}];
+            return [{ res: 2, msg: `Could not retrieve logo from url ${tokenInfo.logoUrl}, error ${error}` }];
         }
     }
     if (logoStreamType.toLowerCase() != "image/png") {
-        return [{res: 2, msg: `Logo image must be PNG image (not ${logoStreamType})`}];
+        return [{ res: 2, msg: `Logo image must be PNG image (not ${logoStreamType})` }];
     }
-    res.push({res: 0, msg: `Logo image type is OK (${logoStreamType})`});
+    res.push({ res: 0, msg: `Logo image type is OK (${logoStreamType})` });
 
     if (logoStreamSize > 100000) {
-        res.push({res: 2, msg: `Logo image too large, max 100 kB, current ${logoStreamSize / 1000} kB`});
+        res.push({ res: 2, msg: `Logo image too large, max 100 kB, current ${logoStreamSize / 1000} kB` });
     } else {
-        res.push({res: 0, msg: `Logo image size is OK (${logoStreamSize / 1000} kB)`});
+        res.push({ res: 0, msg: `Logo image size is OK (${logoStreamSize / 1000} kB)` });
     }
 
     try {
         const logoDimension = await imgDimsCalc.get(tokenInfo.logoUrl, tokenInfo.logoStream);
         if (logoDimension.x == 0 && logoDimension.y == 0) {
-            res.push({res: 2, msg: `Could not retrieve logo dimensions`});
+            res.push({ res: 2, msg: `Could not retrieve logo dimensions` });
         } else if (logoDimension.x > 512 || logoDimension.y > 512) {
-            res.push({res: 2, msg: `Logo dimensions too large ${logoDimension.x}x${logoDimension.y}`});
+            res.push({ res: 2, msg: `Logo dimensions too large ${logoDimension.x}x${logoDimension.y}` });
         } else if (logoDimension.x < 64 || logoDimension.y < 64) {
-            res.push({res: 2, msg: `Logo dimensions too small ${logoDimension.x}x${logoDimension.y}`});
+            res.push({ res: 2, msg: `Logo dimensions too small ${logoDimension.x}x${logoDimension.y}` });
         } else {
-            res.push({res: 0, msg: `Logo dimensions OK (${logoDimension.x}x${logoDimension.y})`});
+            res.push({ res: 0, msg: `Logo dimensions OK (${logoDimension.x}x${logoDimension.y})` });
         }
     } catch (error) {
-        res.push({res: 2, msg: `Could not retrieve logo dimensions (${error})`});
+        res.push({ res: 2, msg: `Could not retrieve logo dimensions (${error})` });
     }
 
     return res;
+}
+
+export async function getTokenCirculation(tokenType: string, explorer: string, tokenAddress: string) {
+    try {
+        if (tokenType.toLowerCase() != 'erc20') {
+            // not erc20, not supported
+            return "";
+        }
+        let explorerUrl2 = explorer;
+        if (!explorerUrl2) {
+            // if no explorerUrl, use default
+            explorerUrl2 = explorerUrl(tokenType, tokenAddress);
+            if (!explorerUrl2) {
+                return "";
+            }
+        }
+        // erc20
+        const httpResult = await fetch(explorerUrl2);
+        if (httpResult.status != 200) {
+            return "";
+        }
+        const fragment2 = " addresses</div>";
+        const fragment1 = 'col-md-8">\n';
+        const resultPage = await httpResult.text();
+        const idx2 = resultPage.indexOf(fragment2);
+        if (idx2 < 30) {
+            //console.log("ERROR: framgment2 not found", "etherscanPage", etherscanPage.length, "etherscanUrl", etherscanUrl);
+            return "";
+        }
+        const idx1 = resultPage.substring(idx2 - 30).indexOf(fragment1);
+        //console.log("idx1", idx1);
+        if (idx1 < 0) {
+            //console.log("ERROR: framgment1 not found");
+            return "";
+        }
+        let holdersString = resultPage.substring(idx2 - 30 + idx1 + fragment1.length, idx2);
+        // remove any , or .
+        holdersString = holdersString.replace(',', '').replace('.', '');
+        //console.log(explorerUrl2, holdersString);
+        return holdersString;
+    } catch (error) {
+        return "";
+    }
+}
+
+const CirculationHoldersLimit = 500;
+
+export function checkHoldersLimit(holders: string): {res: number, msg: string} {
+    if (!holders) {
+        // could not check
+        return {res: 1, msg: "Could not obtain no. of holders"};
+    }
+    const holdersNum = parseInt(holders, 10);
+    if (holdersNum >= 0 && holdersNum < CirculationHoldersLimit) {
+        return {res: 2, msg: `Low token circulation: no. of holders is ${holdersNum}, below limit of ${CirculationHoldersLimit}`};
+    }
+    return {res: 0, msg: `Token circulation OK (no. of holders: ${holdersNum})`};
 }
--- assetsLib/src/tokenInput.ts
+++ assetsLib/src/tokenInput.ts
@@ -3,7 +3,9 @@ import {
     normalizeType,
     ImageDimensionsCalculator,
     explorerUrl,
-    AggregateCheckResults
+    AggregateCheckResults,
+    getTokenCirculation,
+    checkHoldersLimit
 } from "./tokenInfo";
 import { isEthereumAddress, toChecksum } from "./eth-address";
 
@@ -60,19 +62,19 @@ export class TokenInput {
 // - a multi-line string with the detailed results
 // - fixed version if can be auto-fixed
 export async function checkTokenInput(tokenInput: TokenInput, imgDimsCalc: ImageDimensionsCalculator): Promise<[number, string, TokenInput | null]> {
-    let res: {res: number, msg: string}[] = [];
+    let res: { res: number, msg: string }[] = [];
     let fixed: TokenInput | null = null;
 
     if (!tokenInput.name) {
-        res.push({res: 2, msg: "Name cannot be empty"});
+        res.push({ res: 2, msg: "Name cannot be empty" });
     } else {
-        res.push({res: 0, msg: "Name is set"});
+        res.push({ res: 0, msg: "Name is set" });
     }
 
     if (!tokenInput.type || !normalizeType(tokenInput.type)) {
-        res.push({res: 2, msg: `Invalid token type ${tokenInput.type}`});
+        res.push({ res: 2, msg: `Invalid token type ${tokenInput.type}` });
     } else {
-        res.push({res: 0, msg: `Token type OK (${tokenInput.type})`});
+        res.push({ res: 0, msg: `Token type OK (${tokenInput.type})` });
     }
 
     const [contrRes, contrFix] = checkTokenInputContract(tokenInput);
@@ -85,101 +87,104 @@ export async function checkTokenInput(tokenInput: TokenInput, imgDimsCalc: Image
     (await checkTokenInputLogo(tokenInput, imgDimsCalc)).forEach(r => res.push(r));
 
     if (!tokenInput.website) {
-        res.push({res: 2, msg: "Website cannot be empty"});
+        res.push({ res: 2, msg: "Website cannot be empty" });
         try {
             const result = await fetch(tokenInput.website);
             if (result.status != 200) {
-                res.push({res: 2, msg: `Website does not exist, status ${result.status}, url ${tokenInput.website}`});
+                res.push({ res: 2, msg: `Website does not exist, status ${result.status}, url ${tokenInput.website}` });
             } else {
-                res.push({res: 0, msg: `Website OK`});
+                res.push({ res: 0, msg: `Website OK` });
             }
         } catch (error) {
-            res.push({res: 2, msg: `Website does not exist, error ${error}, url ${tokenInput.website}`});
+            res.push({ res: 2, msg: `Website does not exist, error ${error}, url ${tokenInput.website}` });
         }
     }
 
     if (!tokenInput.explorerUrl) {
-        res.push({res: 2, msg: "Explorer cannot be empty"});
+        res.push({ res: 2, msg: "Explorer cannot be empty" });
     } else {
         try {
             const result = await fetch(tokenInput.explorerUrl);
             if (result.status != 200) {
-                res.push({res: 2, msg: `ExplorerUrl does not exist, status ${result.status}, url ${tokenInput.explorerUrl}`});
+                res.push({ res: 2, msg: `ExplorerUrl does not exist, status ${result.status}, url ${tokenInput.explorerUrl}` });
             } else {
                 // check if explorer is what we would think
                 const guessedExplorer = explorerUrl(tokenInput.type, tokenInput.contract);
                 if (tokenInput.explorerUrl != guessedExplorer) {
-                    res.push({res: 1, msg: `Recommended ExplorerUrl is ${guessedExplorer} instead of ${tokenInput.explorerUrl}`});
+                    res.push({ res: 1, msg: `Recommended ExplorerUrl is ${guessedExplorer} instead of ${tokenInput.explorerUrl}` });
                     if (!fixed) { fixed = tokenInput.clone(); }
                     fixed.explorerUrl = guessedExplorer;
                 } else {
-                    res.push({res: 0, msg: `ExplorerUrl OK`});
+                    res.push({ res: 0, msg: `ExplorerUrl OK` });
                 }
             }
         } catch (error) {
-            res.push({res: 2, msg: `ExplorerUrl does not exist, error ${error}, url ${tokenInput.explorerUrl}`});
+            res.push({ res: 2, msg: `ExplorerUrl does not exist, error ${error}, url ${tokenInput.explorerUrl}` });
         }
     }
 
     if (!tokenInput.description) {
-        res.push({res: 2, msg: "Short description cannot be empty"});
+        res.push({ res: 2, msg: "Short description cannot be empty" });
     } else {
-        res.push({res: 0, msg: "Short description is present"});
+        res.push({ res: 0, msg: "Short description is present" });
     }
+
+    // circulation
+    const holders = await getTokenCirculation(tokenInput.type, tokenInput.explorerUrl, tokenInput.contract);
+    res.push(checkHoldersLimit(holders));
 
     const [resnum, resmsg] = AggregateCheckResults(res);
     return [resnum, resmsg, fixed];
 }
 
-function checkTokenInputContract(tokenInput: TokenInput): [{res: number, msg: string}, string] {
+function checkTokenInputContract(tokenInput: TokenInput): [{ res: number, msg: string }, string] {
     if (!tokenInput.contract) {
-        return [{res: 2, msg: "Contract/ID cannot be empty"}, null];
+        return [{ res: 2, msg: "Contract/ID cannot be empty" }, null];
     }
     if (tokenInput.type.toLowerCase() === "erc20" || tokenInput.type.toLowerCase() === "bep20") {
         if (!isEthereumAddress(tokenInput.contract)) {
-            return [{res: 2, msg: `Contract is not a valid Ethereum address!`}, null];
+            return [{ res: 2, msg: `Contract is not a valid Ethereum address!` }, null];
         }
         const inChecksum = toChecksum(tokenInput.contract);
         if (inChecksum !== tokenInput.contract) {
-            return [{res: 2, msg: `Contract is not in checksum format, should be ${inChecksum}`}, inChecksum];
+            return [{ res: 2, msg: `Contract is not in checksum format, should be ${inChecksum}` }, inChecksum];
         }
     }
-    return [{res: 0, msg: `Contract/ID is OK`}, null];
+    return [{ res: 0, msg: `Contract/ID is OK` }, null];
 }
 
-async function checkTokenInputLogo(tokenInput: TokenInput, imgDimsCalc: ImageDimensionsCalculator): Promise<{res: number, msg: string}[]> {
-    let res: {res: number, msg: string}[] = [];
+async function checkTokenInputLogo(tokenInput: TokenInput, imgDimsCalc: ImageDimensionsCalculator): Promise<{ res: number, msg: string }[]> {
+    let res: { res: number, msg: string }[] = [];
 
     if (!tokenInput.logoStream || tokenInput.logoStream.length < 10) {
-        return [{res: 2, msg: "Logo image may not be missing"}];
+        return [{ res: 2, msg: "Logo image may not be missing" }];
     }
 
     if (tokenInput.logoStreamType && tokenInput.logoStreamType.toLowerCase() != "image/png") {
-        return [{res: 2, msg: `Logo image must be PNG image (not ${tokenInput.logoStreamType})`}];
+        return [{ res: 2, msg: `Logo image must be PNG image (not ${tokenInput.logoStreamType})` }];
     }
-    res.push({res: 0, msg: `Logo image type is OK (${tokenInput.logoStreamType})`});
+    res.push({ res: 0, msg: `Logo image type is OK (${tokenInput.logoStreamType})` });
 
     if (tokenInput.logoStreamSize > 100000) {
-        res.push({res: 2, msg: `Logo image too large, max 100 kB, current ${tokenInput.logoStreamSize / 1000} kB`});
+        res.push({ res: 2, msg: `Logo image too large, max 100 kB, current ${tokenInput.logoStreamSize / 1000} kB` });
     } else {
-        res.push({res: 0, msg: `Logo image size is OK (${tokenInput.logoStreamSize / 1000} kB)`});
+        res.push({ res: 0, msg: `Logo image size is OK (${tokenInput.logoStreamSize / 1000} kB)` });
     }
 
     try {
         const logoDimension = await imgDimsCalc.get(null, tokenInput.logoStream);
         if (logoDimension.x == 0 && logoDimension.y == 0) {
-            res.push({res: 2, msg: `Could not retrieve logo dimensions`});
+            res.push({ res: 2, msg: `Could not retrieve logo dimensions` });
         } else if (logoDimension.x > 512 || logoDimension.y > 512) {
-            res.push({res: 2, msg: `Logo dimensions too large ${logoDimension.x}x${logoDimension.y}`});
+            res.push({ res: 2, msg: `Logo dimensions too large ${logoDimension.x}x${logoDimension.y}` });
         } else if (logoDimension.x < 64 || logoDimension.y < 64) {
-            res.push({res: 2, msg: `Logo dimensions too small ${logoDimension.x}x${logoDimension.y}`});
+            res.push({ res: 2, msg: `Logo dimensions too small ${logoDimension.x}x${logoDimension.y}` });
         } else {
-            res.push({res: 0, msg: `Logo dimensions OK (${logoDimension.x}x${logoDimension.y})`});
+            res.push({ res: 0, msg: `Logo dimensions OK (${logoDimension.x}x${logoDimension.y})` });
         }
     } catch (error) {
-        res.push({res: 2, msg: `Could not retrieve logo dimensions (${error})`});
+        res.push({ res: 2, msg: `Could not retrieve logo dimensions (${error})` });
     }
-
 
     return res;
 }
